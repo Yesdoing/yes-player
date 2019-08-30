7dce6241dad860dabf3b4cec77670b7888801aa8
Jane Lee <Yesdoing13@gmail.com>
2019-08-30 19:23:25 +0900

    Updates

---
+++ precache-manifest.aedb9e628a0f7e827adaf0df52ade456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e981eefc90e9d6bd34b8c3cb530b4286",
+    "url": "/yes-player/index.html"
+  },
+  {
+    "revision": "5ae4fb918988dd917c2f",
+    "url": "/yes-player/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "5ae4fb918988dd917c2f",
+    "url": "/yes-player/static/js/2.0d0f6777.chunk.js"
+  },
+  {
+    "revision": "323bd152d66fbcfbf17d",
+    "url": "/yes-player/static/js/main.fa438051.chunk.js"
+  },
+  {
+    "revision": "9f99f38faf8ee996662d",
+    "url": "/yes-player/static/js/runtime~main.08d3b36f.js"
+  }
+]);
